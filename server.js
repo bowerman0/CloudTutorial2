bfe927a027bee5175f6571aaa8cc61b5958c6706
Michael Bowerman <michael.bowerman@gmail.com>
2015-04-28 15:56:11 -0500

    Latest updates to lesson.

--- server.js
+++ server.js
@@ -1,11 +1,52 @@
 'use strict';
-var http = require('http')
-var port = process.env.PORT || 1337;
-console.log('Starting up - port is:' + port);
-var resCount = 0;
-http.createServer(function(req, res) {
-  console.log('Response count:' + ++resCount);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-  console.log('Sent response to client.');
-}).listen(port);
+var GoodWinston = require('good-winston');
+var winston = require('winston');
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+server.register({
+register: require('good'),
+  options: {
+    reporters: [
+      new GoodWinston({
+        ops: '*',
+        request: '*',
+        response: '*',
+        log: '*',
+        error: '*'
+      }, winston)
+    ]
+  }
+}, function(err) {
+  if(err) {
+    return server.log(['error'],'good load error: ' + err);
+  }
+});
+
+if( process.env.PORT ) {
+    server.connection({ port: process.env.PORT });
+}
+else {
+    server.connection({ port: 1337 });
+}
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
